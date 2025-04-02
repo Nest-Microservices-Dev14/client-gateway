60b61238626bf7341546c2394d798879d428d108
castillo <fabianboc960@hotmail.com>
2025-04-02 10:12:45 -0500

    feat: :sparkles: agregar configuración de entorno y validación
    Se añadió un archivo .env.template para la configuración del puerto y se implementó la carga de variables de entorno utilizando dotenv. Además, se creó un esquema de validación con Joi para asegurar que las variables de entorno sean correctas. Se actualizaron package.json y package-lock.json para incluir las nuevas dependencias.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { envs } from './config/envs';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('Main client-gateway');
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.setGlobalPrefix('/api');
+
+  await app.listen(envs.PORT);
+  logger.log(`Products Microservice Running on port ${ envs.PORT }`);
 }
 bootstrap();
