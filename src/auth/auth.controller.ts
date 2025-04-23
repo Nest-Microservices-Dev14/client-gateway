6ffd8e758e8a4eb6ca82234928072c9554a7b63e
Fabian Castillo <fabianboc960@hotmail.com>
2025-04-23 10:42:20 -0500

    feat: :sparkles: Actualizar DTOs de auth-ms
    Se introdujo el DTO de auth para gestionar el body que entra en las peticiones de login y register.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,9 @@
-import { Controller, Get, Inject, Post } from '@nestjs/common';
-import { ClientProxy } from '@nestjs/microservices';
-import { firstValueFrom } from 'rxjs';
+import { Body, Controller, Get, Inject, Post } from '@nestjs/common';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
+import { catchError, firstValueFrom } from 'rxjs';
 import { NATS_SERVERS } from 'src/config/service';
+import { LoginUserDto } from './dto/login.user.dto';
+import { RegisterUserDto } from './dto/register.user.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -11,23 +13,21 @@ export class AuthController {
     ) { }
 
     @Post('register')
-    async register() {
-        const response = await firstValueFrom(
-            this.client.send('auth.register.user', {})
-        )
-        return {
-            response
-        }
+    async register(@Body() registerUserDto: RegisterUserDto) {
+        return this.client.send('auth.register.user', registerUserDto).pipe(
+            catchError( (error) => {
+                throw new RpcException(error);
+            })
+        );
     }
 
     @Post('login')
-    async login() {
-        const response = await firstValueFrom(
-            this.client.send('auth.login.user', {})
-        )
-        return {
-            response
-        }
+    login(@Body() loginUserDto: LoginUserDto) {
+        return this.client.send('auth.login.user', loginUserDto).pipe(
+            catchError( (error) => {
+                throw new RpcException(error);
+            })
+        );
     }
 
     @Get('verify')
